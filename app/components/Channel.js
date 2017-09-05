f3d90a22462e985471b921e1fd69c899934e0bda
I. Saber <isaber23@outlook.com>
2017-09-05 17:44:02 +0100

    more ui improvements

--- app/components/Channel.js
+++ app/components/Channel.js
@@ -12,16 +12,28 @@ function Channel(props) {
             language
         } = props;
         
+        const default_logo = logo === null ? 'https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_300x300.png': logo;
+        console.log(default_logo);
     return (
         <div className="channel">
-            <img src={logo} alt={`Logo of ${display_name}`} />
-            <h1>{display_name}</h1>
+            <header>
+                <img src={default_logo} alt={`Logo of ${display_name}`} />
+                <h1>{display_name}</h1>
+            </header>
             <div className="content">
-                <span>Followers: {followers}</span>
-                <span>Views: {views}</span>
-                <span>Game: {game}</span>
-                <span>Language: {language}</span> 
-                <p>{status}</p>          
+                <div className="fol-and-views">
+                <span><i className="fa fa-users" /> {followers}</span>
+                   <span> <i className="fa fa-eye" /> {views}</span>
+                </div>
+               <div className="played-game">
+                    <i className="fa fa-gamepad" /><span> {game}</span>
+                </div>
+                <div className="language">
+                    <i className="fa fa-language" /> <span> {language.toUpperCase()}</span> 
+                </div>
+                <div className="channel-status">
+                    <p>{status}</p>    
+                </div>      
             </div>
         </div>
     )
